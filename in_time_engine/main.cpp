6e181e103d91e93be8fc75f646fe56b1f8a06eef
Christopher Barrios Agosto <christopherbarriosagosto@gmail.com>
2025-12-12 21:27:53 -0500

    Add project files.

---
+++ in_time_engine/main.cpp
@@ -0,0 +1,8 @@
+// Dependencies | std
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	std::cout << "Program operating" << std::endl;
+
+	std::cout << "Program terminating" << std::endl;
+}
