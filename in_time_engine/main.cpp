d3edc172c7f0f5a152245bb73e271a6abef9738a
Christopher Barrios Agosto <christopherbarriosagosto@gmail.com>
2025-12-14 00:35:06 -0500

    glew dependency
    commiting and pushing to make sure glew does not end in the GitHub repo.

---
+++ in_time_engine/main.cpp
@@ -0,0 +1,12 @@
+// Dependencies | std
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	std::cout << "Program operating" << std::endl;
+
+	std::string helloWorld{ "Hello world!!!" };
+	std::cout << helloWorld << std::endl;
+
+	std::cout << "Program terminating" << std::endl;
+}
